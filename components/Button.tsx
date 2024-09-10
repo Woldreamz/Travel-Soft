b33f6ed2885ae41bb75e947339aa17415855453d
Rainer Nsa <woldreamz@gmail.com>
2024-09-10 13:15:47 +0100

    Initial commit

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,26 +1,12 @@
-import ButtonSvg from "../components/ui/ButtonSvg";
+import { Button as ShadcnButton } from "../components/ui/button";
 
-const Button = ({ className, href, onClick, children, px, white }) => {
-  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
-    px || "px-7"
-  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
-  const spanClasses = "relative z-10";
-
-  const renderButton = () => (
-    <button className={classes} onClick={onClick}>
-      <span className={spanClasses}>{children}</span>
-      {ButtonSvg(white)}
-    </button>
-  );
-
-  const renderLink = () => (
-    <a href={href} className={classes}>
-      <span className={spanClasses}>{children}</span>
-      {ButtonSvg(white)}
-    </a>
-  );
-
-  return href ? renderLink() : renderButton();
-};
+const Button = ({ children, ...props }) => (
+  <ShadcnButton
+    {...props}
+    className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+  >
+    {children}
+  </ShadcnButton>
+);
 
 export default Button;
